95c928be80a9803dbe63069376ec15261b035ee3
Pirun Seng <sengpirun.rain@gmail.com>
2018-03-22 14:04:44 +0700

    Add missing schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210014111) do
+ActiveRecord::Schema.define(version: 20180130105106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 20171210014111) do
     t.text "text", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: false
+    t.integer "availability", default: 0
+    t.integer "age", default: 0
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter", default: ""
+    t.text "body", default: ""
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
